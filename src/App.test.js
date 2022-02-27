43c6c3e6c8e8ed47fbcdb4ea90b9bd80b767a3b4
robert <robert.sobieszek@proexe.pl>
2022-02-27 16:18:38 +0100

    test: Add simple test for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// React Query
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
+
+/**
+ * I have little to no comercial experience with testing components.
+ * Only unit testing with simple logic.
+ * Probably better integration test are what you wanted to see :(
+ */
+describe("Elements rendered by App", () => {
+  it("should render header", () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    );
+    const header = screen.getByRole("banner");
+    expect(header).toBeInTheDocument();
+  });
+
+  it("should render loader", () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    );
+    const loader = screen.getByRole("img", { name: /loader/i });
+    expect(loader).toBeInTheDocument();
+  });
 });
