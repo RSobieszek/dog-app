7df3141f14392c4679eb5a47760e20dd42302a91
robert <robert.sobieszek@proexe.pl>
2022-02-27 16:18:17 +0100

    refactor: Refactor Card component

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -4,14 +4,14 @@ import { oneOfType, arrayOf, node, string } from "prop-types";
 // Import components
 import { Title } from "components";
 
-const Card = ({ title, children }) => {
+function Card({ title, children }) {
   return (
     <div className="m-4 p-5 border-[0.5px] border-gray-200 flex flex-col gap-4">
       <Title text={title} smallDecoration />
       {children}
     </div>
   );
-};
+}
 
 Card.propTypes = {
   title: string.isRequired,
